7b32f6c2dfb43d2b2db5886841a0da26cededd22
Remberto <rembertobeltran@icloud.com>
2022-03-23 22:06:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+    const newNames = []
+    for (let i = 0; i < names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        newNames.push(message);
+        console.log(newNames)
+  }
+
+  return newNames;
+}
+
+writeCards(names);
+
+function countDown(number) {
+    for(let i = 0; i <= number; i++) {
+        console.log(i)
+    }
+}
+
+countDown(10); 
+
+
+function calledWithExactly(number) {
+    for(let i = 0; i <= number; i++) {
+        console.log(i)
+    }
+}
+
+calledWithExactly(4)
